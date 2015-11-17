b96367222bbb38e377add0716952c07073571b70
10npierre <g00210584@W213-06.academic.raritanval.edu>
2015-11-16 20:38:24 -0500

    More Changes

---
+++ WarBattle.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * Write a description of class WarBattle here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class WarBattle
+{
+    // instance variables - replace the example below with your own
+    private ArrayList<Creature> army1;
+    private ArrayList<Creature> army2;
+    private Random random = new Random();
+
+    /**
+     * Constructor for objects of class WarBattle
+     * @param    creatureNumber    installes how many creature are being created
+     */
+    public WarBattle(int creatureNumber)
+    {
+        // initialise instance variables
+    }
+}
