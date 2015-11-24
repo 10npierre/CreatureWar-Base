97bc9d4984dcf70f3d43f6349dba1d1e3625f590
10npierre <nathanalec@aol.com>
2015-11-24 07:41:30 -0800

    changes

--- CreatureCreation.java
+++ CreatureCreation.java
@@ -11,17 +11,19 @@ public class CreatureCreation
 {
     // instance variables - replace the example below with your own
     private Random random = new Random();
-    private ArrayList<Creature> army = new ArrayList<Creature>();
+    private ArrayList<String> army = new ArrayList<String>();
     
 
     /**
      * Constructor for objects of class CreatureCreation
      */
-    public CreatureCreation(Creature human)
+    public CreatureCreation()
     {
         // initialise instance variables
-        army.add(human);
-        
+        army.add("cyberDemon");
+        army.add("human");
+        army.add("balrog");
+        army.add("elf");
     }
 
     /**
@@ -30,9 +32,27 @@ public class CreatureCreation
      * @param  y   a sample parameter for a method
      * @return     the sum of x and y 
      */
-    public int sampleMethod(int y)
+    public Creature makeCreature()
     {
-        // put your code here
-        return y;
+        Creature madeCreature;
+        int numberIndex = random.nextInt(army.size());
+        String creation = army.get(numberIndex);
+        switch (creation) {
+            case "human":       madeCreature = new Human(25,10);
+                                break;
+                             
+            case "cyberDemon":  madeCreature = new CyberDemon(25,10);
+                                break;
+                                
+            case "elf":         madeCreature = new Elf(25,10);
+                                break;
+                                
+            case "balrog":      madeCreature = new Balrog(25,10);
+                                break;
+            
+            default:             madeCreature = null;
+                                break;
+        }
+        return madeCreature;
     }
 }
